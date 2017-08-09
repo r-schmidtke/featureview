ac09af4f5ec47209ffb4649c25e1802a62b8119c
r-schmidtke <robin@schmidtke-ct.de>
2017-08-09 13:39:53 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,8 +4,8 @@ Redmine::Plugin.register :featureview do
   author 'Robin Schmidtke'
   description 'specialized views for product features'
   version '0.0.1'
-
-    permission :featureview, { :featureview => [:index, :show] }, :public => true
+    
+    project_module :featureview
     menu :project_menu, :featureview, { :controller => 'featureview', :action => 'index' }, :caption => :label_featureview, :after => :activity, :param => :project_id
     settings :default => {}, :partial => 'settings/featureview_settings'
 end
