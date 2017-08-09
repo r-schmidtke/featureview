8df9f2960e5f2093566c0112dc815bea9ed34317
r-schmidtke <robin@schmidtke-ct.de>
2017-08-09 13:43:39 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -5,7 +5,9 @@ Redmine::Plugin.register :featureview do
   description 'specialized views for product features'
   version '0.0.1'
     
-    project_module :featureview
-    menu :project_menu, :featureview, { :controller => 'featureview', :action => 'index' }, :caption => :label_featureview, :after => :activity, :param => :project_id
-    settings :default => {}, :partial => 'settings/featureview_settings'
+  project_module :featureview do
+    permission :featureview, { :featureview => [:index, :show] }, :public => true
+  end
+  menu :project_menu, :featureview, { :controller => 'featureview', :action => 'index' }, :caption => :label_featureview, :after => :activity, :param => :project_id
+  settings :default => {}, :partial => 'settings/featureview_settings'
 end
